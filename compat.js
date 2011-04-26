9f45144ae92c151cd5faccbb4c679a3b5cd6facc
Ryan Berdeen <ryan@ryanberdeen.com>
2011-04-25 22:46:13 -0400

    start mp3 parser

---
+++ compat.js
@@ -0,0 +1,125 @@
+(function () {
+var typedArrays = [Int8Array, Uint8Array, Int16Array, Uint16Array, Int32Array, Uint32Array, Float32Array];
+if (window.Float64Array) {
+    typedArrays.push(Float64Array);
+}
+
+// fix for webkit pre slice rename
+if (!Float32Array.prototype.subarray) {
+    typedArrays.forEach(function (cls) {
+        cls.prototype.subarray = cls.prototype.slice;
+    });
+}
+// fix for https://bugzilla.mozilla.org/show_bug.cgi?id=637643
+else if (new Int8Array([0, 1, 0]).subarray(1).subarray(1)[0]) {
+    function subarray (begin, end) {
+        if (arguments.length === 0) {
+            // duplicate the array
+            return new this.constructor(this.buffer, this.byteOffset, this.length);
+        }
+        else {
+            if (begin < 0) {
+                // relative to end
+                begin += this.length;
+            }
+            // clamp to 0, length
+            begin = Math.max(0, Math.min(this.length, begin));
+            if (arguments.length < 2) {
+                // slice to end
+                end = this.length;
+            }
+            else {
+                if (end < 0) {
+                    // relative to end
+                    end += this.length;
+                }
+                // clamp to begin, length
+                end = Math.max(begin, Math.min(this.length, end));
+            }
+
+            var byteOffset = this.byteOffset + begin * this.BYTES_PER_ELEMENT;
+            return new this.constructor(this.buffer, byteOffset, end - begin);
+        }
+    }
+    typedArrays.forEach(function (cls) {
+        cls.prototype.subarray = subarray;
+    });
+}
+
+if (!FileReader.prototype.readAsArrayBuffer) {
+    FileReader.prototype.readAsArrayBuffer = function readAsArrayBuffer () {
+        this.readAsBinaryString.apply(this, arguments);
+        this.__defineGetter__('resultString', this.__lookupGetter__('result'));
+        this.__defineGetter__('result', function () {
+            var string = this.resultString;
+            var result = new Uint8Array(string.length);
+            for (var i = 0; i < string.length; i++) {
+                result[i] = string.charCodeAt(i);
+            }
+            return result.buffer;
+        });
+    };
+}
+
+
+if (!window.DataView) {
+    function DataView(buffer) {
+        this.buffer = buffer;
+    }
+
+    // DataView polyfill
+    DataView.prototype = {
+        get: function (cls, byteOffset, littleEndian) {
+            var buffer;
+            if (!littleEndian) {
+                var sourceChars = new Uint8Array(this.buffer, byteOffset, cls.BYTES_PER_ELEMENT);
+                var destChars = new Uint8Array(cls.BYTES_PER_ELEMENT);
+                if (cls.BYTES_PER_ELEMENT === 2) {
+                    destChars[0] = sourceChars[1];
+                    destChars[1] = sourceChars[0];
+                }
+                else if (cls.BYTES_PER_ELEMENT === 4) {
+                    destChars[0] = sourceChars[3];
+                    destChars[1] = sourceChars[2];
+                    destChars[2] = sourceChars[1];
+                    destChars[3] = sourceChars[0];
+                }
+                else if (cls.BYTES_PER_ELEMENT === 8) {
+                    destChars[0] = sourceChars[7];
+                    destChars[1] = sourceChars[6];
+                    destChars[2] = sourceChars[5];
+                    destChars[3] = sourceChars[4];
+                    destChars[4] = sourceChars[3];
+                    destChars[5] = sourceChars[2];
+                    destChars[6] = sourceChars[1];
+                    destChars[7] = sourceChars[0];
+                }
+                buffer = destChars.buffer;
+            }
+            else if (byteOffset % cls.BYTES_PER_ELEMENT) {
+                var sourceChars = new Uint8Array(this.buffer, byteOffset, cls.BYTES_PER_ELEMENT);
+                var destChars = new Uint8Array(cls.BYTES_PER_ELEMENT);
+                destChars.set(sourceChars);
+                buffer = destChars.buffer;
+            }
+            else {
+                buffer = this.buffer
+            }
+            return new cls(buffer, 0, 1)[0];
+        },
+
+        getUint32: function (byteOffset, littleEndian) {
+            return this.get(Uint32Array, byteOffset, littleEndian);
+        },
+
+        getUint16: function (byteOffset, littleEndian) {
+            return this.get(Uint16Array, byteOffset, littleEndian);
+        },
+        
+        getUint8: function (byteOffset) {
+            return new Uint8Array(this.buffer, byteOffset, 1)[0];
+        }
+    };
+    window.DataView = DataView;
+}
+})();
---
+++ mp3.js
@@ -0,0 +1,264 @@
+function bytesToString(bytes) {
+    return String.fromCharCode.apply(String, Array.prototype.slice.apply(bytes));
+}
+function extractAsciiString(buffer, start, length) {
+    return bytesToString(new Uint8Array(buffer, start, length));
+}
+
+function extractUtf16String(buffer, start, length) {
+    var sourceChars = new Uint8Array(buffer, start, length);
+    // TODO only copy if necessary
+    var chars = new Uint8Array(sourceChars.length);
+    chars.set(sourceChars);
+
+    // TODO handle BOM
+    var values = new Uint16Array(chars.buffer);
+
+    return decodeUtf16(values.subarray(1)); 
+}
+
+// http://www.ietf.org/rfc/rfc2781.txt
+function decodeUtf16(w) {
+    var i = 0;
+    var len = w.length;
+    var w1, w2;
+    var charCodes = [];
+    while (i < len) {
+        var w1 = w[i++];
+        if ((w1 & 0xF800) !== 0xD800) { // w1 < 0xD800 || w1 > 0xDFFF
+            charCodes.push(w1);
+            continue;
+        }
+        if ((w1 & 0xFC00) === 0xD800) { // w1 >= 0xD800 && w1 <= 0xDBFF
+            throw new RangeError('Invalid octet 0x' + w1.toString(16) + ' at offset ' + (i - 1));
+        }
+        if (i === len) {
+            throw new RangeError('Expected additional octet');
+        }
+        w2 = w[i++];
+        if ((w2 & 0xFC00) !== 0xDC00) { // w2 < 0xDC00 || w2 > 0xDFFF)
+            throw new RangeError('Invalid octet 0x' + w2.toString(16) + ' at offset ' + (i - 1));
+        }
+        charCodes.push(((w1 & 0x3ff) << 10) + (w2 & 0x3ff) + 0x10000);
+    }
+    return String.fromCharCode.apply(String, charCodes);
+}
+
+function MpegFrame() {}
+
+function Mp3Reader (blob) {
+    var BUFFER_SIZE = 4096;
+    var AUDIO_VERSIONS = [2.5, -1, 2, 1];
+    var LAYERS = [-1, 3, 2, 1];
+    var SAMPLING_RATES = [[11025, -1, 22050, 44100],
+                          [12000, -1, 24000, 48000],
+                          [8000 , -1, 16000, 32000],
+                          [0,     -1,     0,     0]];
+    var BITRATES = [[null,
+                     [  0,   8,  16,  24,  32,  40,  48,  56,  64,  80,  96, 112, 128, 144, 160],   // V2.5 L3
+                     [  0,   8,  16,  24,  32,  40,  48,  56,  64,  80,  96, 112, 128, 144, 160],   // V2.5 L2
+                     [  0,  32,  48,  56,  64,  80,  96, 112, 128, 144, 160, 176, 192, 224, 256]],  // V2.5 L1
+                    null,
+                    [null,
+                     [  0,   8,  16,  24,  32,  40,  48,  56,  64,  80,  96, 112, 128, 144, 160],   // V2 L3
+                     [  0,   8,  16,  24,  32,  40,  48,  56,  64,  80,  96, 112, 128, 144, 160],   // V2 L2
+                     [  0,  32,  48,  56,  64,  80,  96, 112, 128, 144, 160, 176, 192, 224, 256]],  // V2 L1
+                    [null,
+                     [  0,  32,  40,  48,  56,  64,  80,  96, 112, 128, 160, 192, 224, 256, 320],   // V1 L3
+                     [  0,  32,  48,  56,  64,  80,  96, 112, 128, 160, 192, 224, 256, 320, 384],   // V1 L2
+                     [  0,  32,  64,  96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448]]]; // V1 L1
+
+    var CHANNEL_MODES;
+    var SLOT_SIZES = [-1, 1, 1, 4];
+    this.blob = blob;
+    //this.id3Parser = new Id3Parser(blob);
+    
+    var reader;
+    var offset = 49444;
+    
+    var frames = [];
+    this.frames = frames;
+    
+    var data = new Uint8Array(0);
+    var i = 0;
+    
+    function read(frameCallback) {
+        var done = false;
+        while (true) {
+            if (offset >= blob.length) {
+                console.log('done')
+                return;
+            }
+            if (i >= data.length) {
+                offset += i;
+                reader = new FileReader();
+                reader.onloadend = function(e) {
+                    data = new Uint8Array(e.target.result);
+                    i = 0;
+                    read(frameCallback);
+                }
+                reader.readAsArrayBuffer(blob.slice(offset, BUFFER_SIZE));
+                break;
+            }
+            // TODO ensure we have a full header + side info
+            if (data[i] === 0xff && (data[i + 1] & 0xe0) === 0xe0) {
+                var frame = readHeader(i);
+                if (!frame.valid) {
+                    console.log('invalid frame header');
+                    return;
+                }
+                done = !frameCallback(frame);
+                frames.push(frame);
+                i += frame.size;
+                if (done) {
+                    break;
+                }
+            }
+            else {
+                console.log('missing sync');
+                return;
+            }
+        }
+        
+        function readHeader(pos) {
+            var a = data[pos + 1];
+            var b = data[pos + 2];
+            var c = data[pos + 3];
+
+            var audioVersionId =    (a & 0x18) >> 3;
+            var layerIndex =        (a & 0x06) >> 1;
+            var protection =        (a & 0x01);
+            var bitrateIndex =      (b & 0xf0) >> 4;
+            var samplingRateIndex = (b & 0x0c) >> 2;
+            var paddingBit =        (b & 0x02) >> 1;
+            var privateBit =        (b & 0x01);
+            var channelModeIndex =  (c & 0xc0) >> 6;
+            var modeExtension =     (c & 0x30) >> 4;
+            var copyrightBit =      (c & 0x08) >> 3;
+            var originalBit =       (c & 0x04) >> 2;
+            var emphasis =          (c & 0x03);
+            
+            var valid = true;
+            if (audioVersionId === 1) {
+                valid = false;
+            }
+            else if (layerIndex === 0) {
+                valid = false;
+            }
+            else if (bitrateIndex === 15) {
+                valid = false;
+            }
+            else if (samplingRateIndex === 3) {
+                valid = false;
+            }
+            else if (emphasis === 2) {
+                valid = false;
+            }
+            
+            var version, layer, bitrate, samplingRate, samples, slotSize, frameSize, mainDataBegin;
+            if (valid) {
+                version = AUDIO_VERSIONS[audioVersionId];
+                layer = LAYERS[layerIndex];
+                bitrate = BITRATES[audioVersionId][layerIndex][bitrateIndex];
+                samplingRate = SAMPLING_RATES[samplingRateIndex][audioVersionId];
+                if (layer === 1){
+                    samples = 384;
+                }
+                else if (layer === 2) {
+                    samples = 1152;
+                }
+                else {
+                    if (version === 1) {
+                        samples = 1152;
+                    }
+                    else {
+                        samples = 576;
+                    }
+                }
+                slotSize = layer === 1 ? 4 : 1;
+                frameSize = Math.floor(((samples / 8 * bitrate * 1000) / samplingRate + slotSize * paddingBit) / slotSize) * slotSize;
+                
+                var mainDataBeginOffset = pos + 4 + protection * 2;
+                var mainDataBegin = data[mainDataBeginOffset] << 8 + data[mainDataBeginOffset] & 0x80;
+            }
+            
+            var frame = new MpegFrame();
+            frame.offset = pos + offset;
+            frame.valid = valid;
+            frame.size = frameSize;
+            frame.version = version;
+            frame.layer = layer;
+            frame.protection = !!protection;
+            frame.bitrate = bitrate;
+            frame.samplingRate = samplingRate;
+            frame.padding = paddingBit;
+            frame.privateBit = !!privateBit;
+            frame.channelModeIndex = channelModeIndex; // TODO convert to channel mode?
+            frame.modeExtension = modeExtension; // TODO  ?
+            frame.copyright = !!copyrightBit;
+            frame.original = !!originalBit;
+            frame.samples = samples;
+            frame.mainDataBegin = mainDataBegin;
+
+            return frame;
+        }
+    }
+
+    this.read = read;
+    
+    var blobOffset = offset;
+    
+    function getBuffer(samples, context, callback) {
+        var start = blobOffset;
+        console.log(start);
+        var length = 0;
+        var samplesSoFar = 0;
+        function frameCallback(frame) {
+            length += frame.size;
+            blobOffset += frame.size;
+            samplesSoFar += frame.samples;
+
+            if (samplesSoFar >= samples) {
+                var chunkReader = new FileReader();
+                chunkReader.onloadend = function(e) {
+                    var buffer = context.createBuffer(e.target.result, false);
+                    callback(buffer);
+                };
+                chunkReader.readAsArrayBuffer(blob.slice(start, length, 'audio/mp3'));
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        read(frameCallback);
+    }
+    
+    this.getBuffer = getBuffer;
+    
+    var empty = new Float32Array(0);
+    var currentBuffer = empty;
+    
+    function fill(buffer, offset, callback) {
+        var written = 0;
+        if (currentBuffer.length > 0) {
+            if (buffer.length < currentBuffer.length) {
+                buffer.set(currentBuffer.subarray(0, buffer.length));
+                currentBuffer = currentBuffer.subarray(buffer.length);
+                callback(buffer);
+                return;
+            }
+            else {
+                buffer.set(currentBuffer);
+                written = currentBuffer.length;
+                currentBuffer = empty;
+            }
+        }
+    }
+}
+
+
+
+function showFrameInfo(frame) {
+    console.log('at ' + frame.offset, 'audioVersion', frame.version, 'layer', frame.layer, 'protection?', frame.protection, 'mainDataBegin', frame.mainDataBegin, 'bytes', frame.size, 'next', frame.offset + frame.size);
+}
